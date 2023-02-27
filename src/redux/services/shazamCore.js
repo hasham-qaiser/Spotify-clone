7417dd2c10dce33dabe2a52a26b0a0998748d73b
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2023-02-26 17:40:43 -0800

    songcard with data fetch working

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,12 +1,20 @@
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "44feaf607bmshfd51d0b4de8b6d4p1cd0cfjsnceb664c97f90",
-    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
-  },
-};
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "44feaf607bmshfd51d0b4de8b6d4p1cd0cfjsnceb664c97f90"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
